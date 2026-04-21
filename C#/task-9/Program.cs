505ca63c244dcb996c599997be486667f4983287
davhsi <davish.std@gmail.com>
2026-04-21 16:24:44 +0530

    task-9: reflection and custom attributes

---
+++ C#/task-9/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+
+// Define a custom attribute [Runnable] that can be applied to methods.
+[AttributeUsage(AttributeTargets.Method)]
+public class RunnableAttribute : Attribute
+{
+}
+
+public class Tasks
+{
+    [Runnable]
+    public void Task1()
+    {
+        Console.WriteLine("Task1 executed");
+    }
+
+    [Runnable]
+    public void Task2()
+    {
+        Console.WriteLine("Task2 executed");
+    }
+
+    public void Task3()
+    {
+        Console.WriteLine("This method should not be executed");
+    }
+}
+
+public class MoreTasks
+{
+    [Runnable]
+    public void Task4()
+    {
+        Console.WriteLine("Task4 executed");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Get current assembly
+        // Assembly is a class in the System.Reflection namespace that represents a .NET assembly,
+        // which is a compiled code library used for deployment, versioning, and security.
+        // It contains metadata about the types defined in the assembly, as well as the code itself.
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // here we are using reflection to get all types and methods in the assembly,
+        // and check if they have the [Runnable] attribute.
+        foreach (var type in assembly.GetTypes())
+        {
+            foreach (var method in type.GetMethods())
+            {
+                // Check if method has [Runnable]
+                if (method.GetCustomAttribute(typeof(RunnableAttribute)) != null)
+                {
+                    // Create instance of class
+                    var instance = Activator.CreateInstance(type);
+
+                    // Invoke method
+                    method.Invoke(instance, null);
+                }
+            }
+        }
+    }
+}
---
+++ C#/task-9/README.md
@@ -0,0 +1,6 @@
+### TASK-9: Reflection and Custom Attributes
+- Build an application that discovers and executes methods based on custom attributes.
+- Define a custom attribute (e.g., [Runnable]).
+- Create several classes with methods decorated with the [Runnable] attribute.
+- Use reflection to scan the current assembly for methods marked with [Runnable].
+- Invoke the discovered methods dynamically and display their outputs.
