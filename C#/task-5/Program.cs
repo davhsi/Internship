1e76b12718e9619caa829df04067dcb5cef8b7d5
davhsi <davish.std@gmail.com>
2026-04-21 12:13:15 +0530

    task-5: file handling

---
+++ C#/task-5/Program.cs
@@ -0,0 +1,55 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string inputFile = "input.csv";
+        string outputFile = "output.csv";
+
+        try
+        {
+            string[] lines = File.ReadAllLines(inputFile);
+            string header = lines[0];
+            int totalRows = lines.Length -1;
+
+            int eligibleVoters = 0;
+
+            for(int i = 1; i < lines.Length; i++)
+            {
+                string[] columns = lines[i].Split(',');
+                string name = columns[0];
+                int age = int.Parse(columns[1]);
+                string city = columns[2];
+
+                if(age >= 18)
+                {
+                    eligibleVoters++;
+                }
+            }
+
+            string result = $"CSV Analysis Report\n" +
+                $"Total Records : {totalRows}\n" +
+                $"Eligible Voters: {eligibleVoters}\n";
+
+                File.WriteAllText(outputFile, result);
+
+                Console.WriteLine($"Results saved to {outputFile}");
+
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"Error: {inputFile} not found.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Invalid format");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Error: Missing columns in csv");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Error; {ex.Message}");
+        }
+    }
+}
---
+++ C#/task-5/README.md
@@ -0,0 +1,6 @@
+#### TASK-5: File I/O and Exception Handling
+- Develop an application that reads from and writes to files.
+- Read text from a file (e.g., a log file or a simple CSV).
+- Process the data (for example, count words or lines).
+- Write the result to a new file.
+- Implement exception handling to manage file-related errors (such as FileNotFoundException or IOException).
